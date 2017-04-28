9c3099489776ff42891a888b248b2d37c7b40c5f
Sara Xiang <saraxiang@Saras-MacBook-Pro-2.local>
2017-04-27 23:03:33 -0400

    some tweaking...

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Main from './Main'; // Our custom react component
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Main from './Main';
+
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+const App = () => (
+  <MuiThemeProvider>
+    <Main />
+  </MuiThemeProvider>
+);
+
 // Render the main app react component into the app div.
 // For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-render(<Main />, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
